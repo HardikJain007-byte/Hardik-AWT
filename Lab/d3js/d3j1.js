35a6a140afa0cb4d8fea93cc7483fab095cd1f91
HardikJain007-byte <Royhardik04@gmail.com>
2025-11-28 11:36:55 +0530

    commit

---
+++ Lab/d3js/d3j1.js
@@ -0,0 +1,15 @@
+var data = [1, 2, 10, 32, 33];
+
+var posts = [
+    "post 1 some random data",
+        "post 2 some random data",
+
+            "post 3 some random data",
+
+                "post 4 some random data",
+
+                    "post 5 some random data"
+
+];
+
+ts =d3
